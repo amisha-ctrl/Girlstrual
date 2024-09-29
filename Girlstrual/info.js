e3ff33458b83a368e7cb2dab05ca6ae2a210fa2e
amisha-ctrl <139635382+amisha-ctrl@users.noreply.github.com>
2024-09-29 15:37:49 +0530

    Delete Girlstrual directory

--- Girlstrual/info.js
+++
@@ -1,6 +0,0 @@
-function showCycle(phase) {
-    document.querySelectorAll('.phase').forEach(function(element) {
-        element.style.display = 'none';
-    });
-    document.getElementById(phase).style.display = 'block';
-}
